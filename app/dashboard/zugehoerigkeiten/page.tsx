e4ee4d7b1f71ceb86f5348bbe98730810a958962
Wieber <sebastian.wieber@icloud.com>
2025-04-08 15:12:26 +0200

    Fixes

--- app/dashboard/zugehoerigkeiten/page.tsx
+++ app/dashboard/zugehoerigkeiten/page.tsx
@@ -1,4 +1,4 @@
 "use client"
 export default function ZugehoerigkeitenPage() {
-  return <div className="p-6 text-lg">🔗 Zugehörigkeiten</div>
+  return <div className="p-6 text-lg">🔗 Zugehörigkeitenen</div>
 }
