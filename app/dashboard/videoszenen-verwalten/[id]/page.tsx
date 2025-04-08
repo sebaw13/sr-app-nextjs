ab1e14d08284b883d8b7224c4094bde3ce83dd6a
Wieber <sebastian.wieber@icloud.com>
2025-04-08 21:55:22 +0200

    Fixes

--- app/dashboard/videoszenen-verwalten/[id]/page.tsx
+++ app/dashboard/videoszenen-verwalten/[id]/page.tsx
@@ -12,7 +12,8 @@ import { Separator } from '@/components/ui/separator'
 import { Toggle } from '@/components/ui/toggle'
 import SignedVideoPlayer from '@/components/SignedVideoPlayer'
 
-export default async function Page({ params }: { params: { id: string } }) {
+
+export default async function Page({ params }: any) {
 
   const supabase = await createClient()
 
